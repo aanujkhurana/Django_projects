c4f95b52606c71ab8a2d09459a42a60f2168721f
aanujkhurana <aanujkhurana@gmail.com>
2025-05-17 12:35:29 +1000

    first_app

---
+++ firstDjango/list/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def list(request):
+    return HttpResponse("THIS IS LIST PAGE")
+
+def text(request):
+    return HttpResponse("hello")
